4888befb226f3f9b1b05e6b8be11e9a594f089a4
vvizzard <r.haritiana.z@gmail.com>
2019-07-23 10:04:42 +0300

    remake 2

--- src/main/java/org/mlp/apps/occurrence/OccurrenceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.mlp.apps.occurrence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OccurrenceRepository extends JpaRepository<Occurrence, Integer> {
-
-}
--- src/main/java/org/mlp/apps/species/SpeciesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.mlp.apps.species;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeciesRepository extends JpaRepository<Species, Integer> {
-
-}
